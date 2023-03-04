c71b8d4bb19ccc7914b0e1a36af1b9676ef740b1
nagyb3 <nagybence2003@gmail.com>
2023-03-04 12:32:57 +0100

    card media query, svg background for main

---
+++ main.js
@@ -0,0 +1,7 @@
+const firstCard = document.querySelector('div.card.first');
+
+firstCard.addEventListener('click', () => {
+    window.open('https://nagyb3.github.io/todo', '_blank');
+    //TODO: FIX THE LINK
+});
+
